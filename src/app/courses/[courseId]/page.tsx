db7e216ea794a01f34ba2cf15af0cf85cdd585bb
ARKRAJ BISWAS <arkraj.biswas6@gmail.com>
2026-05-05 15:16:03 +0530

    Update page.tsx

--- src/app/courses/[courseId]/page.tsx
+++ src/app/courses/[courseId]/page.tsx
@@ -2,6 +2,13 @@ import React from 'react';
 import CoursesPageClient from '../components/CoursesPageClient';
 import { courseData } from '../components/courseData';
 
+// This tells Next.js which course pages to build
+export function generateStaticParams() {
+  return [
+    { courseId: 'climate-finance-101' }
+  ];
+}
+
 export default function CoursePage() {
   return <CoursesPageClient course={courseData} />;
 }

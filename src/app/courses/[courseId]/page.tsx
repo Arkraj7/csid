94b44d4f02a137c19d2288e1d8dff672204d2e2a
ARKRAJ BISWAS <arkraj.biswas6@gmail.com>
2026-05-05 15:11:10 +0530

    Remove static params for Next.js course page
    Removed static parameters generation for course IDs.

--- src/app/courses/[courseId]/page.tsx
+++ src/app/courses/[courseId]/page.tsx
@@ -2,15 +2,6 @@ import React from 'react';
 import CoursesPageClient from '../components/CoursesPageClient';
 import { courseData } from '../components/courseData';
 
-// THE FIX: We tell Next.js exactly which course IDs to pre-build for GitHub Pages
-export function generateStaticParams() {
-  return [
-    { courseId: 'climate-finance-101' } // Matches the ID in your courseData.ts
-    // If you add more courses later, just add their IDs here!
-  ];
-}
-
 export default function CoursePage() {
-  // We pass the course data perfectly to the client
   return <CoursesPageClient course={courseData} />;
 }

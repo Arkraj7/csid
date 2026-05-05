60494805878431e5671edd69159b6dabf5b29989
ARKRAJ BISWAS <arkraj.biswas6@gmail.com>
2026-05-05 14:52:58 +0530

    Update page.tsx

--- src/app/courses/[courseId]/page.tsx
+++ src/app/courses/[courseId]/page.tsx
@@ -3,6 +3,6 @@ import CoursesPageClient from '../components/CoursesPageClient';
 import { courseData } from '../components/courseData';
 
 export default function CoursePage() {
-  // We explicitly pass the courseData into the Client component here
+  // We are now passing the course data perfectly to the client!
   return <CoursesPageClient course={courseData} />;
 }
